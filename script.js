22b920b1d33bf4a3c8d1ee11b39deb8aff3c32d0
pwscoding <137758232+pwscoding@users.noreply.github.com>
2024-10-31 19:05:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-    const showHackedMessage = true; // Set this variable to true or false
+    const showHackedMessage = false; // Set this variable to true or false
     const customMessage = [
         "Hey there!",
         "you are not allowed to access this website",
